a409c6bb2d082f67a9850546cf78cc0e73fc24a5
vedant <vedant@ssipmt.com>
2024-05-30 16:21:18 +0530

    Create Course added

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -74,6 +74,11 @@ function HomeLayout({ children }) {
                                 <Link to="/admin/dashboard"> Admin Dashboard</Link>
                             </li>
                         )}
+                        {isLoggedIn && role == 'ADMIN' && (
+                            <li>
+                                <Link to="/course/create"> Create Course</Link>
+                            </li>
+                        )}
 
                         <li>
                             <Link to="/courses" >All Courses</Link>
