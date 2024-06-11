26623dc4850e3e7755898fd8472dec1938e12780
vedant <vedant@ssipmt.com>
2024-06-11 19:38:25 +0530

    reset

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({ 
   server: {
     port: 3005, 
   },
   plugins: [react()],
-  base : '/'
 })
